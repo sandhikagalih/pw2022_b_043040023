e269711018cc5b6ab5c06e5b585f2fb9c5fded2c
sandhikagalih <sandhikagalih@gmail.com>
2022-02-26 11:25:08 +0700

    Mengerjakan latihan di pertemuan 3

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,14 @@
+<?php 
+// Pertemuan 3
+// Membahas mengenai Struktur Kendali
+
+// Pengulangan
+for($i = 1; $i <= 10; $i++) {
+  echo 'Hello World!';
+  echo '<br>';
+}
+
+
+// Pengkondisian
+
+?>
